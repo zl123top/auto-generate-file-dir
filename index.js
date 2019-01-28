b91f8355cb9e826588ecc138a0166438c1aa66a8
zhanglin <a1107108799@gmail.com>
2019-01-28 14:26:31 +0800

    1.0.2

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const path = require('path');
 
 const workUrl = process.cwd(); // 当前工作目录
 const param = process.argv[2] || `demo`; // 第一个参数
-let templateUrl = `./template`;
+let templateUrl2 = `./template`;
+const templateUrl = path.join(__dirname, '/template');
 // if(process.argv[2]){
 //   templateUrl = `${workUrl}/${process.argv[2]}`;
 // }else{
@@ -23,17 +24,19 @@ function copyDir(path1, path2){
   fs.readdir(path1, {
     withFileTypes: true
   },(err, files) => {
-    files.forEach( val => {
-      const Outpath1 = `${path1}/${val.name}`;
-      const Outpath2 = `${path2}/${val.name}`;
-      if(val.isFile()){
-        //console.log(path1, path2)
-        fs.copyFile(`${Outpath1}`, `${Outpath2}`, () => {})
-      }else if(val.isDirectory()){
-        fs.mkdir(`${Outpath2}`, () => {
-          copyDir(Outpath1, Outpath2);
-        });
-      }
-    });
+    if(Array.isArray(files)){
+      files.forEach( val => {
+        const Outpath1 = `${path1}/${val.name}`;
+        const Outpath2 = `${path2}/${val.name}`;
+        if(val.isFile()){
+          //console.log(path1, path2)
+          fs.copyFile(`${Outpath1}`, `${Outpath2}`, () => {})
+        }else if(val.isDirectory()){
+          fs.mkdir(`${Outpath2}`, () => {
+            copyDir(Outpath1, Outpath2);
+          });
+        }
+      });
+    }
   });
 } 
